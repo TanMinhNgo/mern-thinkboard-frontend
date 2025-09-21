7311153283730b7ec255004f9a57cb910811a89d
NgoMinhTan <ngominhtan1252004@gmail.com>
2025-09-22 01:18:09 +0700

    sasw

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // in production, there's no localhost so we have to make this dynamic
-const BASE_URL = import.meta.env.MODE === "development" ? `${import.meta.env.BACKEND_URL}/api` : "http://localhost:8080/api";
+const BASE_URL = import.meta.env.BACKEND_URL;
 
 const api = axios.create({
   baseURL: BASE_URL,
